12be761462be465dba117b14684a861a9aaf26ee
AlbertoAncilotto <ancilottoalberto@gmail.com>
2023-05-08 10:05:27 +0200

    added matisse, afremov and pencil models

--- e03_face_replace.py
+++ e03_face_replace.py
@@ -13,7 +13,7 @@ from keypad import GPIOPinReader
 
 
 class FaceReplace:
-    def __init__(self, height=480, width=320, cam=None, window_name=None, static_bg=None, face_area=None, style_path=None, softer_mask = 'resources/mask.jpg'):
+    def __init__(self, height=480, width=320, cam=None, window_name=None, static_bg=None, face_area=None, style_path=None, style_w=200, style_h=200, softer_mask = 'resources/mask.jpg'):
         self.width = width
         self.heigth = height
         self.gpio = GPIOPinReader()
@@ -25,12 +25,12 @@ class FaceReplace:
         self.people_seg = SegmentationModel()
         self.face_det = FaceDetector()
         if style_path is not None:
-            self.style_model = StyleTransfer(style_path, height=200, width=200, preserve_color=True)
+            self.style_model = StyleTransfer(style_path, height=style_h, width=style_w, preserve_color=True)
         else:
             self.style_model = None
         self.snap_camera = SnapCamera()
 
-        self.softer_mask = cv2.imread(softer_mask)/255.0
+        self.softer_mask = cv2.imread(softer_mask)/255.0 if softer_mask is not None else None
 
         if face_area is None:
             boxes = self.face_det.find_single_face(self.background)
@@ -80,16 +80,19 @@ class FaceReplace:
     def merge_images(self, boxes, frame, softer_mask=None):
         crop_size = (self.face_area[2]-self.face_area[0], self.face_area[3]-self.face_area[1])
         box = boxes[0]
+
         crop = frame[box[1]:box[3], box[0]:box[2]]
         crop = cv2.resize(crop, crop_size)
-        if self.style_model is not None:
-            crop =  self.style_model.transfer_style(crop)
         bg_crop = self.background[self.face_area[1]:self.face_area[3], self.face_area[0]:self.face_area[2]]
-        
-        crop = exposure.match_histograms(crop, bg_crop, channel_axis=2)
 
         self.people_seg.segment(crop)
         crop = self.people_seg.apply(crop, bg_crop)
+        
+        try:
+            crop = exposure.match_histograms(crop, bg_crop, channel_axis=2) #skimage 0.20
+        except:
+            crop = exposure.match_histograms(crop, bg_crop, multichannel=True) #older skimage
+
         styled_frame = self.background.copy()
         
         if softer_mask is not None:
@@ -97,10 +100,14 @@ class FaceReplace:
             styled_frame[self.face_area[1]:self.face_area[3],self.face_area[0]:self.face_area[2]] = (crop*mask + bg_crop*(1-mask)).astype(np.uint8)
         else:
             styled_frame[self.face_area[1]:self.face_area[3],self.face_area[0]:self.face_area[2]] = crop
+
+        
+        if self.style_model is not None:
+            styled_frame =  self.style_model.transfer_style(styled_frame)
         return styled_frame
         
 
 if __name__=='__main__':
     # sn = FaceReplace(static_bg='resources/mona_lisa.png', face_area=[0.27, 0.145, 0.61, 0.55])
-    sn = FaceReplace(static_bg='resources/gogh_self_portrait.jpeg', style_path='style_transfer/gogh_bg_200x200.onnx', face_area=[0.28, 0.18, 0.63, 0.64])
+    sn = FaceReplace(static_bg='resources/gogh_self_portrait.jpeg', style_path='style_transfer/gogh_bg_300x480.onnx', style_w=300, style_h=480, face_area=[0.29, 0.22, 0.6, 0.64])
     sn.face_replace_snap()
